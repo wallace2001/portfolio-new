4a8d2b616b5c2baeb6312fb9bcd03fbaa28ed84b
wallace <wallace.silva@nscreen.com.br>
2025-02-14 15:07:07 -0300

    feat:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Providers } from "./providers/authProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallace Portfolio",
+  title: "Portfolio | Wallace",
   description: "Portfolio Wallace",
 };
 
